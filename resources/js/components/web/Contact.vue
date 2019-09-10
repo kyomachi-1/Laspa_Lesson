0f23674e5b22f036d1cdef699e28913d19e094aa
kyomachi-1 <kyomachi.so.dev1@gmail.com>
2019-09-10 11:52:23 +0900

    add Contact.vue

---
+++ resources/js/components/web/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <div class="jumbotron mt-4">
+            <h1 class="display-4">About Pages</h1>
+            <p class="lead">It's SPA Sample. Developed by Laravel/Vue.js</p>
+            <hr class="my-4">
+            <router-link to="/about" >What's About?</router-link>
+            <router-link to="/contact" class="btn btn-primary btn-lg" role="button">Contact Us</router-link>
+        </div>
+    </div>
+</template> 
